93265c0884eecf145dbb7fcb499759cc7c64744a
Olya Zainkovskaia <o.zainkovskaia@gmail.com>
2026-05-11 12:52:17 +0200

    Fix Taski static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'backend_static' / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
